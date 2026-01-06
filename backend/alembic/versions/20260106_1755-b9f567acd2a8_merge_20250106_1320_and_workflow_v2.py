95598f77c0a66b826745d1d5b9c8ccab2c0e950f
CATrainer <caleb.a.trainer@gmail.com>
2026-01-06 17:56:14 +0000

    fix: merge alembic heads for workflow_v2 and ai_view_filtering

---
+++ backend/alembic/versions/20260106_1755-b9f567acd2a8_merge_20250106_1320_and_workflow_v2.py
@@ -0,0 +1,24 @@
+"""merge_20250106_1320_and_workflow_v2
+
+Revision ID: b9f567acd2a8
+Revises: 20250106_1320, workflow_v2_schema
+Create Date: 2026-01-06 17:55:38.346496
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b9f567acd2a8'
+down_revision = ('20250106_1320', 'workflow_v2_schema')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
